b1622c56bf83bc835611dafbf0676c4a830a92a2
IX68JP <osewa.jozefzoon@nn.nl>
2018-01-15 15:47:05 +0100

    Created board, trying first game

--- boardSetup.py
+++ boardSetup.py
@@ -26,42 +26,42 @@ class HexagonGenerator(object):
       yield x
       yield y
 
-def main():
-  image = Image.new('RGB', (375, 380), 'white')
-  draw = ImageDraw.Draw(image)
-  hexagon_generator = HexagonGenerator(20)
-  r = 10
-  x = 20
-  y = 20
-  
+  def main():
+    image = Image.new('RGB', (375, 380), 'white')
+    draw = ImageDraw.Draw(image)
+    hexagon_generator = HexagonGenerator(20)
+    r = 10
+    x = 20
+    y = 20
+
   for row in range(22):
-	for col in range(11):
-		if not ( (row % 2 == 0 and col == 0) | (row < 3 and col == 1) | \
-			((row < 4 or row == 17) and col == 0) |\
-			((row == 0 or row > 19) and col == 2) |\
-			(row > 17 and row < 22 and col < 2) |\
-			((row == 0 or row == 1) and col > 3) |\
-			((row == 2 or row == 3) and col == 5) |\
-			(col > 5) |\
-			(row >= 17 and col > 4) | (row > 18 and col > 3) | (row > 20 and col > 1) ):
-      			hexagon = hexagon_generator(row, col)
-      			draw.polygon(list(hexagon), outline='red', fill='white')
-  movePiece(draw, 0, 5)
-  image.show()
+    for col in range(11):
+      if not ( (row % 2 == 0 and col == 0) | (row < 3 and col == 1) | \
+        ((row < 4 or row == 17) and col == 0) |\
+        ((row == 0 or row > 19) and col == 2) |\
+        (row > 17 and row < 22 and col < 2) |\
+        ((row == 0 or row == 1) and col > 3) |\
+        ((row == 2 or row == 3) and col == 5) |\
+        (col > 5) |\
+        (row >= 17 and col > 4) | (row > 18 and col > 3) | (row > 20 and col > 1) ):
+        hexagon = hexagon_generator(row, col)
+        draw.polygon(list(hexagon), outline='red', fill='white')
+        movePiece(draw, 0, 5)
+        image.show()
 
-def movePiece(canvas, row, col):
-	r = 10
-	if col % 2 == 0:
-		x = 20 + row * 40
-	else:
-		x = 20 + row * 33
-	y = 40 + col * 30
-	canvas.ellipse((y-r, x-r, y+r, x+r), fill=(0,0,0,255))
+  def movePiece(canvas, row, col):
+      r = 10
+      if col % 2 == 0:
+        x = 20 + row * 40
+      else:
+        x = 20 + row * 33
+        y = 40 + col * 30
+        canvas.ellipse((y-r, x-r, y+r, x+r), fill=(0,0,0,255))
 
-main()
+      main()
 
 
   # Rest of class as previously defined
- # def rows(self, canvas_height):
- #   """Returns the number of rows required to fill the canvas height."""
-   # return int(math.ceil(canvas_height / self.row_height))
+  # def rows(self, canvas_height):
+  #   """Returns the number of rows required to fill the canvas height."""
+  # return int(math.ceil(canvas_height / self.row_height))
